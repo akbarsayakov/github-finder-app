dd003a42af8194ac1e4cc4a30599e5f196738321
unknown <akbarsayakov@gmail.com>
2022-02-09 01:03:28 -0800

    Display Users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
 
 function UserResults() {
   const [users, setUsers] = useState([])
@@ -19,13 +21,15 @@ function UserResults() {
     setUsers(data)
     setLoading(false)
   }
-  return (
-    <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-      {users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  )
+  if (!loading)
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    )
+  else return <Spinner />
 }
 
 export default UserResults
