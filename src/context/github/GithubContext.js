d2ba7e37bf8e4a7236ea39ecd08f0f3978123018
unknown <akbarsayakov@gmail.com>
2022-03-31 02:10:28 -0700

    final commit

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -90,10 +90,7 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        repos: state.repos,
-        loading: state.loading,
+        ...state,
         searchUsers,
         clearResults,
         getUser,
